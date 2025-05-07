d9d01986541449fd6757a342f1c4d983ef4a8dc7
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-07 13:33:10 -0400

    CREACIÓN DE SERVICE

---
+++ src/main/java/com/seccion7/orm/service/PacienteService.java
@@ -0,0 +1,18 @@
+package com.seccion7.orm.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.seccion7.orm.model.Paciente;
+import com.seccion7.orm.repository.PacienteRepository;
+
+@Service
+public class PacienteService {
+    @Autowired
+    private PacienteRepository pacienteRepostiory;
+    public List<Paciente> findAll() {
+        return pacienteRepostiory.findAll();
+    }
+}
