3c7b27988406e5f79dcce295e7a511a3e293592b
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-07 13:33:01 -0400

    CREACIÓN DE REPO

---
+++ src/main/java/com/seccion7/orm/repository/PacienteRepository.java
@@ -0,0 +1,13 @@
+package com.seccion7.orm.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.seccion7.orm.model.Paciente;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Integer>{
+    List<Paciente> findAll();
+}
