b74f0e1266f056d2ec008717b77d7c0a3af72d7c
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-07 13:21:06 -0400

    CREACIÓN DE MODELO

---
+++ src/main/java/com/seccion7/orm/model/Paciente.java
@@ -0,0 +1,43 @@
+package com.seccion7.orm.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// PARA PERSISTENCIA
+@Entity
+@Table(name = "Paciente")
+
+// PARA MANIPULAR OBJECTOS
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 13, nullable = false, unique = true)
+    private String rut;
+
+    @Column(length = 50, nullable = false)
+    private String nombre;
+
+    @Column(length = 50, nullable = false)
+    private String apellido;
+
+    @Column(nullable = false)
+    private LocalDate fNacimiento;
+
+    @Column(length = 250, nullable = false)
+    private String correo;
+}
